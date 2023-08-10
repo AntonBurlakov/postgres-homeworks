2e0f06b58095171cee4102a6780d60a6a04f449e
Burlakov Anton <burlakovanton99@yandex.ru>
2023-08-11 00:21:19 +0300

    hw1

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,22 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+from employees import sql_empl, val_empl
+from customers import sql_cust, val_cust
+from orders import sql_ord, val_ord
+
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="914905"
+)
+
+try:
+    with conn:
+        with conn.cursor() as cur:
+            cur.executemany(sql_empl, val_empl)
+            cur.executemany(sql_cust, val_cust)
+            cur.executemany(sql_ord, val_ord)
+finally:
+    conn.close()
